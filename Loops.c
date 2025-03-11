3898d89061487f51ec41eee8df3310bcfd71bda0
Jmutia17 <jonathanmutia17@gmail.com>
2025-03-11 10:34:58 +0300

    Loops.c

--- Loops.c
+++ Loops.c
@@ -8,7 +8,7 @@ Unit name: Computer programming
 // Using for loop
 #include<stdio.h>
 int main(){
-int i;
+int i=1;
 for (i = 1; i <= 100; i++) {
     printf("Number is: %d and cube of the %d is: %d\n", i, i, i * i * i);
 }
